d07ae7455b7cae053252dbcc0b96b71c4e8c34d2
Jessie Chen <jessiechen0513@gmail.com>
2018-01-12 17:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians,instruments){
 
 function johnLennonFacts(facts){
   var length = facts.length;
-  var i = 0
+  var i = 0;
   while (length > i){
     facts.push(`${facts[i]} + !!!`);
   }
