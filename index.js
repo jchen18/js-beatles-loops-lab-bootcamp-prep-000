08f0a58e84ca19fc67ff3cc450f2a53ef17f234d
Jessie Chen <jessiechen0513@gmail.com>
2018-01-12 15:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
   var pairing = [];
   for (let i = 1; i < 5; i++){
-    var specific_musician = musicians[i];
-    var specific_instrument = instruments[i];
     pairing.push("${i} plays ${i}")
   }
  return pairing 
